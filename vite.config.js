aadc652e9d7a856766947ee6c7cd00d10842bf7a
achmadsrim <43878246+achmadsrim@users.noreply.github.com>
2025-09-17 14:09:08 +0700

    fix vercel config for Vite SPA

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Kalau pakai subdomain Netlify atau custom domain root, biarkan "/"
-  base: '/',
-  build: {
-    outDir: 'dist',
-    sourcemap: false, // matikan sourcemap di production
-    minify: 'esbuild', // cepat & kecil
-    rollupOptions: {
-      output: {
-        manualChunks: undefined, // biar ga kebanyakan split file
-      },
-    },
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
-})
+  base: "/",
+});
